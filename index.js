5d922b0ce0cc2f4355fdeb73f1d8fb71e0d7b0bb
Joy Purkistha <joypurokisthosh@gmail.com>
2023-06-29 02:49:38 +0600

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
@@ -13,12 +13,12 @@ app.use(express.json())
 
 const verifyJwt = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        res.status(401).send({error: true, message: 'unauthorized access'})
+    if (!authorization) {
+        res.status(401).send({ error: true, message: 'unauthorized access' })
     }
     const token = authorization.split(' ')[1]
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-        if(error){
+        if (error) {
             return res.status(403).send({ error: true, message: 'unauthorized access' })
         }
         req.decoded = decoded
@@ -45,24 +45,55 @@ async function run() {
 
         const serviceCollection = client.db('jerinDB').collection('services')
         const reviewCollection = client.db('jerinDB').collection('reviews')
+        const userCollection = client.db('jerinDB').collection('users')
 
 
         // JWT
-        app.post('/token', async(req, res) => {
+        app.post('/token', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '365d'})
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+            res.send({ token })
         })
 
         // Services
-        app.get('/services', async(req, res) => {
+        app.get('/services', async (req, res) => {
             const result = await serviceCollection.find().toArray()
             res.send(result)
         })
 
         // Reviews
-        app.get('/reviews', async(req, res) => {
+        app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+        // User
+
+        app.get('/users', verifyJwt, async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(query, updateDoc);
             res.send(result)
         })
 
